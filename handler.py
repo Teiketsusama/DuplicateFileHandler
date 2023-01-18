dc1ab9b347b01f5ebb8263b7817fd52369589778
june <djie627@gmail.com>
2023-01-19 00:18:34 +1100

    Stage 3/4 add hash

--- handler.py
+++ handler.py
@@ -1,16 +1,16 @@
 import os
 import sys
+import hashlib
 
 
-# a root directory, a file extension, and a sort order as input
-def find_duplicates(root_dir, file_ext, sort_order):
+def get_size_paths(root_dir: str, file_ext: str, reverse: bool):
     file_groups = {}
 
     # os.walk function to iterate through all files and subdirectories of the root directory
-    for dirpath, dirnames, filenames in os.walk(root_dir):
+    for dirpath, _, filenames in os.walk(root_dir):
         for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
             if not file_ext or filename.endswith(file_ext):
-                file_path = os.path.join(dirpath, filename)
                 file_size = os.path.getsize(file_path)
                 # checks if the current file's size is already a key in the file_groups dictionary
                 if file_size in file_groups:
@@ -19,32 +19,68 @@ def find_duplicates(root_dir, file_ext, sort_order):
                 else:
                     # the file is added as the first item in the list associated with that size as the key
                     file_groups[file_size] = [file_path]
+    return dict(sorted(file_groups.items(), key=lambda x: x[0], reverse=reverse))
 
-    # key=lambda x: x[0] is a lambda function used as a sorting key. It tells sorted() to sort the key-value pairs by
-    # the first element of each pair, which is the size
-    if sort_order == "2":
-        sorted_groups = sorted(file_groups.items(), key=lambda x: x[0])
-    elif sort_order == "1":
-        sorted_groups = sorted(file_groups.items(), key=lambda x: x[0], reverse=True)
-    else:
-        print("Wrong option")
-        return
 
-    for group in sorted_groups:
-        print(f"{group[0]} bytes")
-        for file in group[1]:
+def show_size_paths(file_groups: dict):
+    for size, group in file_groups.items():
+        print(f"{size} bytes")
+        for file in group:
             print(file)
         print()
 
 
+def get_size_hash_paths(file_groups: dict):
+    hash_groups = {}
+    for file_size, file_paths in file_groups.items():
+        if len(file_paths) > 1:
+            hash_groups[file_size] = {}
+            for file in file_paths:
+                with open(file, "rb") as f:
+                    file_hash = hashlib.md5(f.read()).hexdigest()
+                if file_hash in hash_groups[file_size]:
+                    hash_groups[file_size][file_hash].append(file)
+                else:
+                    hash_groups[file_size][file_hash] = [file]
+    return hash_groups
+
+
+def show_size_hash_paths(hash_groups: dict):
+    file_count = 1
+    for file_size, hashes in hash_groups.items():
+        print(f"\n{file_size} bytes")
+        for hash, file_paths in hashes.items():
+            if len(file_paths) > 1:
+                print(f"Hash: {hash}")
+                for file in file_paths:
+                    print(f"{file_count}. {file}")
+                    file_count += 1
+
+
+def main():
+    file_ext = input("Enter file format:\n")
+    print("Size sorting options:", "1. Descending", "2. Ascending\n", sep="\n")
+    while (sort_order := input("Enter a sorting option:\n")) not in "12":
+        print("Wrong option\n")
+    reverse = sort_order == "1"
+
+    sorted_groups = get_size_paths(root_dir, file_ext, reverse)
+    show_size_paths(sorted_groups)
+
+    while (check_duplicates := input("\nCheck for duplicates?\n")) not in ("yes", "no"):
+        print("Wrong option\n")
+    check_duplicates = check_duplicates == "yes"
+
+    if check_duplicates:
+        hash_groups = get_size_hash_paths(sorted_groups)
+        show_size_hash_paths(hash_groups)
+
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Directory is not specified")
-    else:
+    try:
         root_dir = sys.argv[1]
-        file_ext = input("Enter file format:")
-        print("Size sorting options:")
-        print("1. Descending")
-        print("2. Ascending")
-        sort_order = input("Enter a sorting option:")
-        find_duplicates(root_dir, file_ext, sort_order)
+    except IndexError:
+        print("Directory is not specified")
+        sys.exit()
+    else:
+        main()
